3550a829e20066d5cd7443461e822919e052bd30
jonathan-m6 <jonathanney14@gmail.com>
2022-11-06 21:06:52 -0600

    predicciones correspondiente al evento

--- api/controllers/events_controller.py
+++ api/controllers/events_controller.py
@@ -12,5 +12,9 @@ async def get_all_events(user_id:str):
     events=list(mongo_provider.db.events.find())
     predictions=list(mongo_provider.db.predictions.find({'user_id':user_id}))
     
+
+    for item in predictions:
+        get_indexes = [i for i, value in enumerate(events) if value['_id'] == item['eventId']]
+        events[get_indexes[0]]['prediction']=item
     
     return events
