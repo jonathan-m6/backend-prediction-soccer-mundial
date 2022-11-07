a14681db7b29838d01a225bcf9fe46df9150b1f4
jonathan-m6 <jonathanney14@gmail.com>
2022-11-06 22:30:45 -0600

    controller usuario listo

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from api.controllers import events_controller
 from api.controllers import predictions_controller
+from api.controllers import users_controller
 
 router=APIRouter()
 router.include_router(events_controller.router)
 router.include_router(predictions_controller.router)
+router.include_router(users_controller.router)
