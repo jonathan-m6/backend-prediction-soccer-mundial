7c1d80966a221e47c603fb66aa15312ccba5fe97
joaquin_n6 <joaquinne87@gmail.com>
2022-11-09 00:15:31 -0600

    endpoints users

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -18,3 +18,24 @@ async def user_post(contract: user):
   elif not (user['username']==contract.username and user['email']==contract.email):
     raise HTTPException(409,"Usuario ya existe")
   return user
+
+@router.post("/{id_usuario}/amigo", status_code=status.HTTP_201_CREATED)
+async def user_put(id_usuario:str, body: dict):
+  friend=mongo_provider.db.users.find_one({'username': body['username']})
+  if not friend:
+    raise HTTPException(409,"Usuario no existente")
+  
+  user=mongo_provider.db.users.find_one({'_id': id_usuario})
+  user['amigos'].append(friend['username'])
+
+  mongo_provider.db.users.update_one({'_id': id_usuario}, {'$set': user})
+  return user
+
+@router.get("/{id_usuario}/table", status_code=status.HTTP_201_CREATED)
+async def table_get(id_usuario:str, body: dict):
+  user = mongo_provider.db.users.find_one({'_id': id_usuario})
+  arrayTable = user['amigos']
+  arrayTable.append(user['username'])
+
+  users = mongo_provider.db.users.find({'username': {'$in': arrayTable}}).sort({'total': -1})
+  return users
--- models/user.py
+++ models/user.py
@@ -4,3 +4,8 @@ class user(BaseModel):
   _id: str
   email: str = Field(...)
   username: str = Field(...)
+  puntosResultado: int = Field(default = 0)
+  puntosMarcador: int = Field(default = 0)
+  total: int = Field(default = 0)
+  amigos: list = Field(default = [])
+  avatar: str = Field(default=0)
