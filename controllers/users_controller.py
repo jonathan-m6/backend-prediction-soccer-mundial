ac2e1dc40daf0eb732f49b58895330871062313e
jonathan-m6 <jonathanney14@gmail.com>
2022-11-18 11:02:06 -0600

    quitando esapcios

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -11,19 +11,20 @@ router = APIRouter(
 
 @router.post("")
 async def user_post(contract: user):
-  user=mongo_provider.db.users.find_one({'username':contract.username.lower()})
+  userNameContract=contract.username.lower().replace(" ","")
+  user=mongo_provider.db.users.find_one({'username':userNameContract})
   if not user:
     user ={**contract.dict(), '_id': shortuuid.uuid()}
-    user['username'] = user['username'].lower()
+    user['username'] = userNameContract
     user['email'] = user['email'].lower()
     mongo_provider.db.users.insert_one(user)
-  elif not (user['username'].lower()==contract.username.lower() and user['email'].lower()==contract.email.lower()):
+  elif not (user['username'].lower()==userNameContract and user['email'].lower()==contract.email.lower()):
     raise HTTPException(409,"Usuario ya existe")
   return user
 
 @router.put("/{id_usuario}/amigo")
 async def user_put(id_usuario:str, body: dict):
-  friend=mongo_provider.db.users.find_one({'username': body['username'].lower()})
+  friend=mongo_provider.db.users.find_one({'username': body['username'].lower().replace(" ","")})
   user=mongo_provider.db.users.find_one({'_id': id_usuario})
   if not 'amigos' in user:
     user["amigos"]=[]
