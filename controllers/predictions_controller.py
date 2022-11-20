126506010dcf51c69111480d6aac403b3e636530
jonathan-m6 <jonathanney14@gmail.com>
2022-11-20 06:58:19 -0600

    predicciones despues de la hora

--- controllers/predictions_controller.py
+++ controllers/predictions_controller.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import APIRouter, status, HTTPException
 from models.prediction import prediction
 import core.var_mongo_provider as mongo_provider
@@ -11,11 +12,21 @@ router = APIRouter(
 
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def prediction_post(contract: prediction):
+	fecha_hoy=datetime.utcnow()
+	event=mongo_provider.db.events.find_one({'_id':contract.eventId})
+	if fecha_hoy>=event["fechaOrder"]:
+		responseError={ "_id":"","eventId":"","golesLocal":0,"golesVisita":0,"userId":"","tiemposExtra":False,"penales":False}
+		return responseError
 	prediccion ={**contract.dict(), '_id': shortuuid.uuid()}
 	mongo_provider.db.predictions.insert_one(prediccion)
 	return prediccion
 
 @router.put("/{id_prediction}")
 async def prediction_put(id_prediction:str,contract:prediction):
+	fecha_hoy=datetime.utcnow()
+	event=mongo_provider.db.events.find_one({'_id':contract.eventId})
+	if fecha_hoy>=event["fechaOrder"]:
+		responseError={ "_id":"","eventId":"","golesLocal":0,"golesVisita":0,"userId":"","tiemposExtra":False,"penales":False}
+		return responseError
 	mongo_provider.db.predictions.update_one({'_id':id_prediction }, {'$set': contract.dict()})
 	return {**contract.dict(), '_id': id_prediction}
